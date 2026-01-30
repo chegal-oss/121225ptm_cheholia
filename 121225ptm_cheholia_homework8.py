2e3d0f9070055601e3955318ec613451b0eb2530
chegal <chegal@inbox.ru>
2026-01-30 11:09:24 +0100

    homework 8

--- 121225ptm_cheholia_homework8.py
+++ 121225ptm_cheholia_homework8.py
@@ -2,24 +2,23 @@ _DEBUG = True
 for task in range(1, 5):
     match task:
         case 1:
-            # 1 Проверка кодировки
+            print("\n1. Проверка кодировки")
             symbol = "A" if _DEBUG else input("Введите символ: ")
             print("Код Unicode = ", ord(symbol))
             print("ASCII символ: ", bool(ord(symbol) < 128))
         case 2:
-            #2 Подстрока с заполнением
+            print("\n2. Подстрока с заполнением")
             string = "Программирование" if _DEBUG else input("Введи строку: ")
-            #first, last = int(input("Введите начальный индекс: ")), int(input("Введите конечный индекс: "))
-            first, last = 3, 20
+            first, last = (3, 20) if _DEBUG else (int(input("Введите начальный индекс: ")), int(input("Введите конечный индекс: ")))
             substring = string[first:last]
             print("Подстрока:", substring + "_" * (last - len(substring)))
         case 3:
-            #3 Подсчёт символа
+            print("\n3. Подсчёт символа")
             string = "Hello, world!" if _DEBUG else input("Введи строку: ")
             symbol = "o" if _DEBUG else input("Введите символ: ")
             print("Символ o встречается", string.count(symbol), "раз(а).")
         case 4:
-            #4 Инвертирование строки без цифр
+            print("\n4. Инвертирование строки без цифр")
             string = "n52uFs6Inoh67ty8P" if _DEBUG else input("Введи строку: ")
             i = len(string)
             parts = []
