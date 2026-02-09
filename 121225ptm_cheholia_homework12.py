5e4e0e65b79a6e1bb66dcc09c0bfb796a7d4b781
chegal <chegal@inbox.ru>
2026-02-09 15:34:56 +0100

    homework 12

--- 121225ptm_cheholia_homework12.py
+++ 121225ptm_cheholia_homework12.py
@@ -1,25 +1,10 @@
-for task in range(1, 4):
-    match task:
-        case 1:
-            print("\nPython Fundamentals 2025: Домашнее задание 12")
-            print("\n1.Сумма положительных чисел")
-            numbers = [1245.4435, -302.1403, 87459.99, -520.8265, 1450.001]
-            print(f"Сумма положительных чисел: ${sum(x for x in numbers if x > 0):.2f}")
+print("\nPython Fundamentals 2025: Домашнее задание 12")
+print("\n1.Сумма положительных чисел")
+numbers = [1245.4435, -302.1403, 87459.99, -520.8265, 1450.001]
+print(numbers)
+print(f"Сумма положительных чисел: ${sum(x for x in numbers if x > 0):.2f}")
 
-        case 2:
-            print("\n2.Счета клиентов")
-            data_list = ["John 23 12345.678", "Alice 30 200.50", "Bob 35 15000.3", "Charlie 40 500.75"]
-            for name, age, balance in [item.split() for item in data_list]:
-                print(f"Имя: {name:10} | Возраст: {age:>3} | Баланс: {float(balance):10.2f}")
-
-
-
-
-
-
-
-
-
-
-
-
+print("\n2.Счета клиентов")
+data_list = ["John 23 12345.678", "Alice 30 200.50", "Bob 35 15000.3", "Charlie 40 500.75"]
+for name, age, balance in [item.split() for item in data_list]:
+    print(f"Имя: {name:10} | Возраст: {age:>3} | Баланс: {float(balance):10.2f}")
