5e4030f9c58930d58c251c05467b7e683b316230
chegal <chegal@inbox.ru>
2026-02-23 12:13:13 +0100

    homework 35

--- 121225ptm_cheholia_homework35.py
+++ 121225ptm_cheholia_homework35.py
@@ -1,52 +1,35 @@
-import random
-
-print("\nPython Fundamentals 2025: Домашнее задание 34")
-print("\n1. Класс Rectangle")
+print("\nPython Fundamentals 2025: Домашнее задание 35")
+print("\nСчётчик экземпляров + Проверка данных пользователя")
 
 
-class Rectangle:
-    def __init__(self, w: int, h: int):
-        self.width: int = w
-        self.height: int = h
+class User:
+    _total_user = 0
 
-    def get_area(self) -> int:
-        return self.width * self.height
+    def __init__(self, username: str, password: str):
+        User._total_user += 1
+        if username and len(password) > 5:
+            self.username = username
+            self.password = password
+            print("Created:", username)
+        else:
+            raise ValueError("Empty username" if not username else f"Invalid password '{password}'")
 
+    def __del__(self):
+        User._total_user -= 1
 
-rectangle = Rectangle(4, 5)
-print(f"Площадь:", rectangle.get_area())
-rectangle.width = 5
-rectangle.height = 7
-print(f"Новая площадь:", rectangle.get_area())
-
-print("\n2. Класс Counter")
-
-
-class Counter:
-    def __init__(self):
-        self._counter = 0
-
-    def add_one(self):
-        self._counter += 1
-        print("Значение увеличено.", self)
-    def sub_one(self):
-        self._counter -= 1
-        print("Значение уменьшено.", self)
-
-    def __gt__(self, other: int):
-        return self._counter > other
-
-    def __str__(self):
-        return f"Текущее значение: {self._counter}"
+    @staticmethod
+    def get_total():
+        return f"Total user: {User._total_user}"
 
 
-counter = Counter()
-for i in range(5):
-    if random.randint(0, 1) and counter > 0:
-        counter.sub_one()
-    else:
-        counter.add_one()
+temp = [("Вася", "qwerty"), ("", "qweqwewq"), ("Вася", "qwe")]
+ref_collector = []
+for n, p in temp:
+    try:
+        ref_collector.append(User(n, p))
+    except ValueError as e:
+        print("Value error:", e)
 
-print(counter)
-
-
+print(User.get_total())
+ref_collector.clear()
+print(User.get_total())
