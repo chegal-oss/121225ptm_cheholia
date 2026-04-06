99e414e8ed22e82fa28ef9082f6fad49bc06ac76
chegal <chegal@inbox.ru>
2026-04-06 10:23:43 +0200

    homework 25

--- 121225ptm_cheholia_homework25.py
+++ 121225ptm_cheholia_homework25.py
@@ -1,24 +1,42 @@
 import logging
+import sys
 
 print("\nPython Fundamentals 2025: Домашнее задание 25")
 
-print("n2. Логирование ошибок")
+class MyMadFunctionException(BaseException):
+    pass
+
+def div_without_error(a: str, b: str) -> float:
+    try:
+        return float(a) / float(b)
+    except ZeroDivisionError:
+        raise MyMadFunctionException("Ошибка: На ноль делить нельзя.")
+    except ValueError:
+        raise MyMadFunctionException("Ошибка: Введено некорректное число.")
+
+
+print("\n2. Логирование ошибок")
 log_format = "%(asctime)s - %(filename)s - %(levelname)s - %(lineno)d - %(message)s"
 logging.basicConfig(
     filename="errors.log",
     format=log_format,
-    level = logging.ERROR
+    level=logging.ERROR
 )
-stream_handler = logging.StreamHandler()
+stream_handler = logging.StreamHandler(sys.stdout)
 stream_handler.setFormatter(logging.Formatter(log_format))
 logging.getLogger().addHandler(stream_handler)
 
+
 print("\n1. Деление без ошибок")
-a, b = "345", "5a"
+a, b = "345.2121", "5a"
 print("Введите делимое:", a)
 print("Введите делитель:", b)
+
 try:
-    print(float(a) / float(b))
-except ValueError:
-    print("Ошибка: Введено некорректное число.")
-    logging.error("Ошибка: Введено некорректное число.")
+    res = div_without_error(a, b)
+except MyMadFunctionException as e:
+    logging.error(e)
+else:
+    print(res)
+
+
