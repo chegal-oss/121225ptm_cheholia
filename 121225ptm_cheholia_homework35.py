7bc1ae785b64293879da6276ed4849543efb4ca0
chegal <chegal@inbox.ru>
2026-05-04 09:09:18 +0200

    homework 35

--- 121225ptm_cheholia_homework35.py
+++ 121225ptm_cheholia_homework35.py
@@ -6,30 +6,26 @@ class User:
     _total_user = 0
 
     def __init__(self, username: str, password: str):
-        User._total_user += 1
-        if username and len(password) > 5:
-            self.username = username
-            self.password = password
-            print("Created:", username)
-        else:
-            raise ValueError("Empty username" if not username else f"Invalid password '{password}'")
+        if not username:
+            raise ValueError("Empty username")
 
-    def __del__(self):
-        User._total_user -= 1
+        if len(password) <= 5:
+            raise ValueError(f"Invalid password {password}")
+
+        self.username = username
+        self.password = password
+        print("Created:", username)
+        User._total_user += 1
 
     @staticmethod
     def get_total():
         return f"Total user: {User._total_user}"
 
 
-temp = [("Вася", "qwerty"), ("", "qweqwewq"), ("Вася", "qwe")]
-ref_collector = []
-for n, p in temp:
+for n, p in (("Вася", "qwerty"), ("", "qweqwewq"), ("Вася", "qwe")):
     try:
-        ref_collector.append(User(n, p))
+        _ = User(n, p)
     except ValueError as e:
         print("Value error:", e)
 
-print(User.get_total())
-ref_collector.clear()
 print(User.get_total())
